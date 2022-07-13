7294e84fff9dc5159bce26a7449029793332c21d
Sammonnay-Sarcar <77871122+Sammonnay-Sarcar@users.noreply.github.com>
2022-07-13 20:39:43 +0530

    first commit

---
+++ easy-shop-zvas-master/models/order-item.js
@@ -0,0 +1,30 @@
+// const mongoose = require('mongoose');
+
+// const orderItemSchema = mongoose.Schema({
+//     quantity: {
+//         type: Number,
+//         required: true
+//     },
+//     product: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'Product'
+//     }
+// })
+
+// exports.OrderItem = mongoose.model('OrderItem', orderItemSchema);
+
+
+const mongoose = require('mongoose');
+
+const orderItemSchema = mongoose.Schema({
+    quantity: {
+        type: Number,
+        required: true
+    },
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    }
+})
+
+exports.OrderItem = mongoose.model('OrderItem', orderItemSchema);
